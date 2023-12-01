a436a0a570468fd076e190bf9cf34ac2ff6cc2f2
Anton Arboleda <anton.arboleda@gmail.com>
2023-12-01 15:37:19 -0800

    add optimization

--- intro_to_c/bitcount.c
+++ intro_to_c/bitcount.c
@@ -4,14 +4,27 @@
     Write a function which given an integer returns 
     the number of "on" bits
 */
-
-int bitcount(int n) {
+// Must be an unsigned int, if you use a 
+// signed int, the right shift will add a 1 at the beginning
+// first bit when right shifting
+int bitcount(unsigned n) {
     int count = 0;
-    while (n > 0) {
-        /* if last bit & 1 is on. Can also do n % 1*/
+    while (n) {
         count += n & 0x01; 
+        /* if last bit & 1 is on. Can also do n % 1*/
         /* shift by 1 */
         n >>= 1;
+    }
+    return count;
+}
+
+// Use the fact that x &= (x-1) deletes the rightmost 1 bit to write a faster 
+// version. 2.9 p.51 in K&R The C Programming Language
+int bitcount_optimized(unsigned n) {
+    int count = 0;
+    while (n) {
+        n &= (n - 1);
+        count++;
     }
     return count;
 }
@@ -21,6 +34,8 @@ int main() {
     assert(bitcount(1) == 1);
     assert(bitcount(7) == 3);
     assert(bitcount(8) == 1);
+    assert(bitcount(-8) == 1);
     assert(bitcount(32) == 1);
-    printf("passed!\n");
+    assert(bitcount(0xffffffff) == 32);
+    printf("OK\n");
 }
