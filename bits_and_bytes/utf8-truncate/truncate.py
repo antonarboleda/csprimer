b4d444ca2f729581ad51575ef4fda9191ca1942b
Anton Arboleda <anton.arboleda@gmail.com>
2024-01-06 13:14:00 -0800

    make checks better

--- bits_and_bytes/utf8-truncate/truncate.py
+++ bits_and_bytes/utf8-truncate/truncate.py
@@ -5,22 +5,13 @@ def is_nth_bit_set(n, byte):
 
 # Helper methods to check if the MSB's are the start of a unicode code point
 def is_len_two(b):
-    for k in range(7, 5, -1):
-        if not is_nth_bit_set(k, b):
-            return False    
-    return not is_nth_bit_set(5, b)
+    return b > 0xc0
 
 def is_len_three(b):
-    for k in range(7, 4, -1):
-        if not is_nth_bit_set(k, b):
-            return False
-    return not is_nth_bit_set(4, b) 
+    return b > 0xe0
 
 def is_len_four(b):
-    for k in range(7, 3, -1):
-        if not is_nth_bit_set(k, b):
-            return False
-    return not is_nth_bit_set(3, b)
+    return b > 0xf0
 
 NEWLINE = ord("\n")
 with open("./cases", "rb") as f:
@@ -28,25 +19,21 @@ with open("./cases", "rb") as f:
     for index, l in enumerate(lines):
         trunc_len = l[0]
         output_line = bytearray([])
-        for i, byte in enumerate(l[1:]):
+        for i, byte in enumerate(l[1:-1]):
             # Truncate if there are too many bytes. Also 
             # check if in the middle of a unicode code point. If so
             # don't add any of the bytes and append a new line
             if i >= trunc_len:
-                output_line.append(NEWLINE)
                 break 
             elif is_len_two(byte) and i + 2 > trunc_len:
-                output_line.append(NEWLINE)
                 break
             elif is_len_three(byte) and i + 3 > trunc_len:
-                output_line.append(NEWLINE)
                 break
             elif is_len_four(byte) and i + 4 > trunc_len:
-                output_line.append(NEWLINE)
                 break
 
             output_line.append(byte)
         try:
-            fout.write(output_line.decode())
+            fout.write(output_line.decode() + "\n")
         except Exception as e:
             print(e)
