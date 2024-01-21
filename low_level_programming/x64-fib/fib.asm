2b1928a168a92eb87a6bb4913613cac0b1a60f2a
Anton Arboleda <anton.arboleda@gmail.com>
2024-01-20 21:17:07 -1000

    v2

--- low_level_programming/x64-fib/fib.asm
+++ low_level_programming/x64-fib/fib.asm
@@ -20,16 +20,17 @@ global rfact
 fib:
 	push rbp ; save rbp to stack
 	push rbx ; save rbx to stack
-	mov ebp, edi ; save value of n to rbp
-	mov eax, edi ; result = n
-	cmp edi, 1 ; n <= 1?
+	mov rbp, rdi ; save value of n to rbp
+	
+	mov eax, edi ; base case: if (n <= 1) return n; 
+	cmp edi, 1
 	jle .done
 
 	sub ebp, 1 ; n - 1
-	mov edi, ebp ; move into 1st arg
+	mov edi, ebp ; move n - 1  into 1st arg
 	call fib ; fib(n - 1)
-	mov ebx, eax ; save result
-	sub ebp, 1 ; todo use lea 
+	mov ebx, eax ; save  result
+	sub ebp, 1 ;
 	mov edi, ebp ; move rbp into first arg
 	call fib ; fib(n - 2)
 	add eax, ebx ; result = fib(n - 1) + fib(n - 2)
@@ -38,6 +39,24 @@ fib:
 	pop rbp
 	ret
 
+fib_v2:
+	mov eax, edi ; base case: if (n <= 1) return n; 
+	cmp edi, 1
+	jle .end
+
+	push rdi ; save n to stack
+	sub edi, 1 ; fib(n - 1) 
+	call fib ; 
+	pop rdi ; pop n from stack back into rdi
+
+	push rax ; push fib(n - 1) to stack as eax will be clobbered
+	sub edi, 2 ; n - 2
+	call fib ; fib(n - 2)
+	pop rcx ; pop return value of fib(n-1) to rcx and ...
+	add eax, ecx ; ... add to return value of fib(n-2)
+.end:
+	ret
+	
 ; practice in order to solve fibonacci
 rfact:
 	push rbx
